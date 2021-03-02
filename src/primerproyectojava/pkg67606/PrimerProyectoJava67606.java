d26a63dbbf87c70f9abedfdce6a473edaa06be49
Campe <Campe@LAPTOP-F5OP52OL>
2021-03-01 22:11:38 -0600

    Primer proyecto Java

---
+++ src/primerproyectojava/pkg67606/PrimerProyectoJava67606.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerproyectojava.pkg67606;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Campe
+ */
+public class PrimerProyectoJava67606 {
+
+    /**
+     * @par
+        int Valor = 18;am args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    int edad = 18;
+    System.out.println("Mi edad es:"+ edad);
+              
+    char caracter = 'a';
+    System.out.println("Mi caracter es:" + caracter);
+    
+    boolean decision = true;
+    System.out.println("Mi decision es:" + decision);
+    
+    boolean decision2 = false;
+    System.out.println("Mi segunda decision es:" + decision2);
+    
+    } 
+}
